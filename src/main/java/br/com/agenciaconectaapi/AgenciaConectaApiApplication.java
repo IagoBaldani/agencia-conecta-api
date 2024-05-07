0b1ae11f55d7ab3e8f57450f1456d53922bf0157
Iago Baldani <66563803+IagoBaldani@users.noreply.github.com>
2024-05-07 11:25:01 -0300

    Initial commit

---
+++ src/main/java/br/com/agenciaconectaapi/AgenciaConectaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenciaconectaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaConectaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgenciaConectaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/agenciaconectaapi/AgenciaConectaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agenciaconectaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaConectaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
