a545e53f0cecfc8218f35cdd0a3f968540f19ba3
Iago Baldani <iagobaldanialmeida@hotmail.com>
2024-06-12 17:36:19 -0300

    FIX - Criacao da classe DatabaseInitializer para execucao do codigo de init do DB para ambiente local Docker.

---
+++ src/main/java/br/com/agenciaconectaapi/config/DatabaseInitializer.java
@@ -0,0 +1,34 @@
+package br.com.agenciaconectaapi.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
+
+    public DatabaseInitializer(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void run(String... args) {
+        // Verifica se o banco de dados está vazio
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM usuarios", Integer.class);
+
+        if (count == null || count == 0) {
+            // Se estiver vazio, execute os scripts SQL
+            ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+            resourceDatabasePopulator.addScript(new ClassPathResource("init-db-agencia_conecta.sql"));
+            resourceDatabasePopulator.execute(dataSource);
+        }
+    }
+}
--- src/main/resources/init-db-agencia_conecta.sql
+++ src/main/resources/init-db-agencia_conecta.sql
@@ -37,18 +37,6 @@ CREATE TABLE IF NOT EXISTS `influenciadores` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
--- Copiando dados para a tabela agencia_conecta.influenciadores: ~9 rows (aproximadamente)
-INSERT INTO `influenciadores` (`id`, `ativo`, `celular`, `cidade_estado`, `cpf`, `data_contrato`, `data_nascimento`, `email`, `endereco`, `instagram`, `nome`, `tiktok`, `youtube`) VALUES
-	(1, b'1', '(11)98765-4321', 'São Paulo/SP', '987.654.321-00', '2021-12-15', '1990-05-12', 'mariaoliveira@example.com', 'Rua das Flores, 456', '@mariaoliveira', 'Maria Oliveira', '@mariaoliveira', 'www.youtube.com/@mariaoliveira'),
-	(2, b'1', '(31)98765-4321', 'Belo Horizonte/MG', '456.123.789-00', '2023-06-10', '1982-11-30', 'joaosantos@example.com', 'Avenida Central, 789', '@joaosantos', 'João Santos', '@joaosantos', 'www.youtube.com/@joaosantos'),
-	(3, b'1', '(41)98765-4321', 'Curitiba/PR', '654.789.123-00', '2024-01-05', '1995-08-15', 'anasouza@example.com', 'Rua das Árvores, 987', '@anasouza', 'Ana Souza', '@anasouza', 'www.youtube.com/@anasouza'),
-	(4, b'1', '(51)98765-4321', 'Porto Alegre/RS', '789.456.123-00', '2022-08-20', '1988-03-05', 'pedrolima@example.com', 'Rua dos Pinheiros, 321', '@pedrolima', 'Pedro Lima', '@pedrolima', 'www.youtube.com/@pedrolima'),
-	(5, b'1', '(32)98765-4321', 'Juiz de Fora/MG', '852.369.741-00', '2023-11-30', '1992-07-10', 'fernandacosta@example.com', 'Rua das Palmeiras, 741', '@fernandacosta', 'Fernanda Costa', '@fernandacosta', 'www.youtube.com/@fernandacosta'),
-	(6, b'1', '(62)98765-4321', 'Goiânia/GO', '369.741.852-00', '2024-04-02', '1980-12-25', 'rafaelapereira@example.com', 'Avenida das Rosas, 852', '@rafaelapereira', 'Rafaela Pereira', '@rafaelapereira', 'www.youtube.com/@rafaelapereira'),
-	(7, b'1', '(21)98765-4321', 'Rio de Janeiro/RJ', '123.456.789-00', '2022-03-25', '1985-09-20', 'carlosilva@example.com', 'Avenida Atlântica, 123', '@carlosilva', 'Carlos Silva', '@carlosilva', 'www.youtube.com/@carlosilva'),
-	(8, b'1', '(84)98765-4321', 'Natal/RN', '258.369.147-00', '2021-10-07', '1993-01-15', 'carolinasantos@example.com', 'Rua das Dunas, 369', '@carolinasantos', 'Carolina Santos', '@carolinasantos', 'www.youtube.com/@carolinasantos'),
-	(9, b'1', '(91)98765-4321', 'Belém/PA', '369.147.258-00', '2024-02-12', '1984-06-28', 'gabrielsilva@example.com', 'Avenida dos Mangueirais, 147', '@gabrielsilva', 'Gabriel Silva', '@gabrielsilva', 'www.youtube.com/@gabrielsilva');
-
 -- Copiando estrutura para tabela agencia_conecta.servicos
 CREATE TABLE IF NOT EXISTS `servicos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -70,27 +58,6 @@ CREATE TABLE IF NOT EXISTS `servicos` (
   KEY `FK19xbsn3eqggxcntnmpcmliowx` (`influenciador_id`),
   CONSTRAINT `FK19xbsn3eqggxcntnmpcmliowx` FOREIGN KEY (`influenciador_id`) REFERENCES `influenciadores` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
--- Copiando dados para a tabela agencia_conecta.servicos: ~19 rows (aproximadamente)
-INSERT INTO `servicos` (`id`, `ativo`, `celular_contratante`, `data_fim`, `data_inicio`, `descricao_tipo_pagamento`, `email_contratante`, `exclusividade`, `impulsionamento`, `nome_contratante`, `porcentagem`, `proposta`, `uso_imagem`, `valor`, `influenciador_id`) VALUES
-	(1, b'1', '(11)33333-2222', '2024-06-15', '2024-05-16', '100% antes', 'contato@betsson.com.br', b'0', b'1', 'Betsson - Apostas Esportivas', 45.00, '3 stories por semana - 1 mês - Destaque', b'0', 7500.00, 8),
-	(2, b'1', '(11)88888-8888', '2024-06-09', '2024-05-10', '50% antes, 50% depois', 'comercial@vipbet.com.br', b'1', b'0', 'VipBet - Apostas Online', 40.00, '3 posts por semana - 1 mês - Destaque', b'1', 7000.00, 5),
-	(3, b'1', '(11)77777-7777', '2024-06-05', '2024-05-15', '100% antes', 'contato@betmaster.com.br', b'0', b'1', 'BetMaster - Apostas Esportivas', 25.00, '1 post por dia - 3 semanas - Destaque', b'0', 3000.00, 3),
-	(4, b'1', '(11)66666-6666', '2024-06-11', '2024-05-12', '70% antes, 30% depois', 'comercial@superbet.com.br', b'0', b'0', 'SuperBet - Apostas Online', 30.00, '4 stories por semana - 1 mês - Destaque', b'1', 6000.00, 8),
-	(5, b'1', '(11)55555-5555', '2024-06-03', '2024-05-20', '100% antes', 'contato@betextreme.com.br', b'1', b'1', 'BetExtreme - Apostas Esportivas', 20.00, '2 posts por semana - 2 semanas - Destaque', b'0', 4000.00, 2),
-	(6, b'1', '(11)44444-4444', '2024-06-16', '2024-05-17', '60% antes, 40% depois', 'comercial@betway.com.br', b'0', b'1', 'BetWay - Apostas Online', 35.00, '1 post por dia - 1 mês - Destaque', b'1', 5500.00, 6),
-	(7, b'1', '(11)33333-3333', '2024-06-13', '2024-05-14', '100% antes', 'contato@betgold.com.br', b'1', b'0', 'BetGold - Apostas Esportivas', 45.00, '3 stories por semana - 1 mês - Destaque', b'0', 7500.00, 4),
-	(8, b'1', '(11)22222-2222', '2024-06-01', '2024-05-11', '80% antes, 20% depois', 'comercial@betfair.com.br', b'1', b'1', 'BetFair - Apostas Online', 30.00, '2 posts por semana - 3 semanas - Destaque', b'1', 6000.00, 1),
-	(9, b'1', '(11)99999-8888', '2024-06-15', '2024-05-16', '100% antes', 'contato@bet365.com.br', b'1', b'1', 'Bet365 - Apostas Esportivas', 25.00, '4 stories por semana - 2 semanas - Destaque', b'1', 5000.00, 9),
-	(10, b'1', '(11)77777-9999', '2024-05-27', '2024-05-13', '90% antes, 10% depois', 'comercial@betsafe.com.br', b'0', b'0', 'BetSafe - Apostas Online', 35.00, '1 post por dia - 2 semanas - Destaque', b'1', 5500.00, 7),
-	(11, b'1', '(11)66666-9999', '2024-06-07', '2024-05-18', '100% antes', 'contato@betsson.com.br', b'0', b'1', 'Betsson - Apostas Esportivas', 40.00, '3 posts por semana - 3 semanas - Destaque', b'1', 7000.00, 8),
-	(12, b'1', '(11)88888-7777', '2024-06-04', '2024-05-21', '70% antes, 30% depois', 'comercial@betwinner.com.br', b'1', b'0', 'BetWinner - Apostas Online', 30.00, '2 stories por dia - 2 semanas - Destaque', b'1', 5500.00, 3),
-	(13, b'0', '(11)77777-8888', '2024-06-13', '2024-05-14', '100% antes', 'contato@betclic.com.br', b'1', b'1', 'BetClic - Apostas Esportivas', 35.00, '3 posts por semana - 1 mês - Destaque', b'1', 6500.00, 6),
-	(14, b'1', '(11)66666-5555', '2024-06-04', '2024-05-15', '80% antes, 20% depois', 'comercial@betfair.com.br', b'0', b'0', 'BetFair - Apostas Online', 25.00, '1 post por dia - 3 semanas - Destaque', b'0', 4000.00, 2),
-	(15, b'1', '(11)55555-4444', '2024-06-03', '2024-05-20', '100% antes', 'contato@bethard.com.br', b'1', b'1', 'BetHard - Apostas Esportivas', 20.00, '2 stories por semana - 2 semanas - Destaque', b'1', 5000.00, 9),
-	(16, b'1', '(11)44444-3333', '2024-06-02', '2024-05-13', '90% antes, 10% depois', 'comercial@bet365.com.br', b'0', b'0', 'Bet365 - Apostas Online', 30.00, '4 posts por semana - 3 semanas - Destaque', b'1', 6000.00, 4),
-	(17, b'1', '(11)33333-2222', '2024-06-15', '2024-05-16', '100% antes', 'contato@betsson.com.br', b'0', b'1', 'Betsson - Apostas Esportivas', 45.00, '3 stories por semana - 1 mês - Destaque', b'0', 7500.00, 8),
-	(18, b'1', '(11)22222-1111', '2024-06-01', '2024-05-17', '70% antes, 30% depois', 'comercial@betway.com.br', b'1', b'0', 'BetWay - Apostas Online', 35.00, '1 post por dia - 2 semanas - Destaque', b'1', 5500.00, 5);
 
 -- Copiando estrutura para tabela agencia_conecta.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
