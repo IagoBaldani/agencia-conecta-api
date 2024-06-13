2360c104eda3cee9c2d7d09d8c711d73a4ef88fd
Iago Baldani <iagobaldanialmeida@hotmail.com>
2024-06-13 12:29:59 -0300

    FIX - Remocao do Flyway e implementacao do Liquibase

--- src/main/resources/db.migration/V1__migracao_data_contrato.sql
+++
@@ -1,5 +0,0 @@
-UPDATE influenciadores
-SET data_assinatura_contrato = data_contrato;
-
-ALTER TABLE influenciadores
-DROP COLUMN data_contrato;
--- src/main/resources/init-db-agencia_conecta.sql
+++ src/main/resources/init-db-agencia_conecta.sql
@@ -19,46 +19,6 @@
 CREATE DATABASE IF NOT EXISTS `agencia_conecta` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci */;
 USE `agencia_conecta`;
 
--- Copiando estrutura para tabela agencia_conecta.influenciadores
-CREATE TABLE IF NOT EXISTS `influenciadores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ativo` bit(1) NOT NULL,
-  `celular` varchar(255) DEFAULT NULL,
-  `cidade_estado` varchar(255) DEFAULT NULL,
-  `cpf` varchar(255) DEFAULT NULL,
-  `data_contrato` date DEFAULT NULL,
-  `data_nascimento` date DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `endereco` varchar(255) DEFAULT NULL,
-  `instagram` varchar(255) DEFAULT NULL,
-  `nome` varchar(255) DEFAULT NULL,
-  `tiktok` varchar(255) DEFAULT NULL,
-  `youtube` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
--- Copiando estrutura para tabela agencia_conecta.servicos
-CREATE TABLE IF NOT EXISTS `servicos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ativo` bit(1) NOT NULL,
-  `celular_contratante` varchar(255) DEFAULT NULL,
-  `data_fim` date DEFAULT NULL,
-  `data_inicio` date DEFAULT NULL,
-  `descricao_tipo_pagamento` varchar(255) DEFAULT NULL,
-  `email_contratante` varchar(255) DEFAULT NULL,
-  `exclusividade` bit(1) NOT NULL,
-  `impulsionamento` bit(1) NOT NULL,
-  `nome_contratante` varchar(255) DEFAULT NULL,
-  `porcentagem` decimal(38,2) DEFAULT NULL,
-  `proposta` varchar(255) DEFAULT NULL,
-  `uso_imagem` bit(1) NOT NULL,
-  `valor` decimal(38,2) DEFAULT NULL,
-  `influenciador_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK19xbsn3eqggxcntnmpcmliowx` (`influenciador_id`),
-  CONSTRAINT `FK19xbsn3eqggxcntnmpcmliowx` FOREIGN KEY (`influenciador_id`) REFERENCES `influenciadores` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
 -- Copiando estrutura para tabela agencia_conecta.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
